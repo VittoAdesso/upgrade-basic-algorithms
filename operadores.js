9e90620dd09aef9f662e1e52472dbb73801dde6e
Vittorio Adesso <adessovittorio8@gmail.com>
2021-10-20 18:37:22 +0200

    Iteración #3

---
+++ operadores.js
@@ -0,0 +1,32 @@
+/* 1.1 Multiplica 10 por 5 y muestra el resultado mediante alert. */
+
+let multiplica = 10 * 5;
+alert(multiplica);
+
+/* 1.1 Multiplica 10 por 5 y muestra el resultado mediante alert. */
+let divide = 10 / 5;
+alert(divide);
+/* 
+1.3 Muestra mediante un alert el resto de dividir 15 por 9. */
+
+alert(15 / 9);
+
+/* 1.4 Usa el correcto operador de asignación que resultará en x = 15, 
+teniendo dos variables y = 10 y z = 5. */
+
+let x;
+let y = 10;
+let z = 5;
+
+x = y + z;
+/* console.log(x);  */
+
+/* 1.5 Usa el correcto operador de asignación que resultará en x = 50,
+    teniendo dos variables y = 10 y z = 5. */
+
+let x;
+let y = 10;
+let z = 5;
+
+x = y * z;
+/* console.log(x);  */
